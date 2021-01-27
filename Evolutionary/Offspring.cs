752121d4641fa7003e7784e12d29f8ecc7f194f5
Peter Wehrfritz <peter.wehrfritz@mailbox.org>
2021-01-28 00:08:40 +0100

    Add documentation to offspring

--- Evolutionary/Offspring.cs
+++ Evolutionary/Offspring.cs
@@ -6,13 +6,40 @@ using Troschuetz.Random;
 
 namespace Evolutionary
 {
+    /// <summary>
+    /// The offspring class.
+    /// </summary>
+    /// <typeparam name="TIndividual">The individuals type.</typeparam>
+    /// <typeparam name="TDataSet">The data set type.</typeparam>
     public class Offspring<TIndividual, TDataSet>
     {
+        /// <summary>
+        /// The enviroment.
+        /// </summary>
         public Enviroment<TIndividual, TDataSet> Enviroment { get; }
+
+        /// <summary>
+        /// The complete sorted list of the parents.
+        /// </summary>
         public IEnumerable<TIndividual> Parents { get; }
+
+        /// <summary>
+        /// A never ending enumeration of a random selection of the parents.
+        /// </summary>
         public IEnumerable<TIndividual> RandomParents { get; }
+
+        /// <summary>
+        /// The list of children of the generation.
+        /// </summary>
         public IEnumerable<TIndividual> Children { get; }
 
+        /// <summary>
+        /// Creates a new offspring.
+        /// </summary>
+        /// <param name="enviroment">The enviroment..</param>
+        /// <param name="parents">The sorted parent collection.</param>
+        /// <param name="randomParents">A never ending random enumeration over the parents.</param>
+        /// <param name="children">The children.</param>
         public Offspring(Enviroment<TIndividual, TDataSet> enviroment, IEnumerable<TIndividual> parents, IEnumerable<TIndividual> randomParents,  IEnumerable<TIndividual> children)
         {
             if (enviroment is null)
