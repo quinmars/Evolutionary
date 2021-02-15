257ffa37afa8fef791df457e69479ca65af79c38
Peter Wehrfritz <peter.wehrfritz@mailbox.org>
2021-02-16 00:30:16 +0100

    Add creations methods for Permutations

--- Evolutionary.Tests/Individuals/PermutationTests.cs
+++ Evolutionary.Tests/Individuals/PermutationTests.cs
@@ -57,6 +57,30 @@ namespace Evolutionary.Tests.Individuals
         }
 
         [Fact]
+        public void CreateRandomly()
+        {
+            var permutation = Permutation.CreateRandomly(5, new TRandom());
+
+            permutation
+                .Should().BeEquivalentTo(0, 1, 2, 3, 4);
+            permutation.IsCircular
+                .Should().BeFalse();
+        }
+        
+        [Fact]
+        public void CreateRandomlyCircular()
+        {
+            var permutation = Permutation.CreateCircularRandomly(5, new TRandom());
+
+            permutation
+                .Should().BeEquivalentTo(0, 1, 2, 3, 4);
+            permutation.IsCircular
+                .Should().BeTrue();
+            permutation
+                .Should().HaveElementAt(0, 0);
+        }
+
+        [Fact]
         public void SwapMutation()
         {
             var array = new int[] { 1, 2, 3, 4, 5, 6 };
---
+++ Evolutionary/Individuals/Permutation.Creation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Troschuetz.Random;
+
+namespace Evolutionary.Individuals
+{
+    public readonly partial struct Permutation
+    {
+        public static Permutation CreateRandomly(int n, TRandom random)
+        {
+            var permutation = new Permutation(n, false);
+            var array = permutation.ToArray();
+
+            array.AsSpan().Scramble(random);
+            return new Permutation(array, permutation.IsCircular);
+        }
+        
+        public static Permutation CreateCircularRandomly(int n, TRandom random)
+        {
+            var permutation = new Permutation(n, true);
+            var array = permutation.ToArray();
+
+            array.AsSpan(1).Scramble(random);
+            return new Permutation(array, permutation.IsCircular);
+        }
+    }
+}
